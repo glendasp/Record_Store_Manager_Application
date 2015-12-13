856071a6b03ec5672164ae47e4095b1e193717db
ShivonQ <realitykeeper@gmail.com>
2015-12-12 23:41:05 -0600

    Most things are fixed now.  Creating the Archived Records Table and view and whatnot.

--- src/com/malcolm/Archived_Sales_Tab.java
+++ src/com/malcolm/Archived_Sales_Tab.java
@@ -1,7 +1,15 @@
 package com.malcolm;
 
+import javax.swing.*;
+
 /**
  * Created by Malcolm on 12/12/2015.
  */
 public class Archived_Sales_Tab {
+    private JTable sales_Archive_Display_JTable;
+    private JButton sortButton;
+    private JComboBox archive_Sort_Options_Combo_Box;
+    private JPanel archived_Sales_Tab;
+
+
 }
